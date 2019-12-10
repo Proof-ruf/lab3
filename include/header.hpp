d35d0a67ab8da353c1bb038db06f75620791867a
Ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-10 22:17:12 +0300

    1.0

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,41 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <clocale>
+#include <atomic>
+#include <map>
+
+using std::atomic_size_t;
+using std::map;
+
+
+template <typename T>
+class SharedPtr {
+public:
+    SharedPtr();
+    SharedPtr(T* ptr);
+    SharedPtr(const SharedPtr& r);
+    SharedPtr(SharedPtr&& r);
+    ~SharedPtr();
+    auto operator=(const SharedPtr& r) -> SharedPtr&;
+    auto operator=(SharedPtr&& r) -> SharedPtr&;
+
+    // проверяет, указывает ли указатель на объект
+    explicit operator bool() const;
+
+    auto operator*() const -> T&;
+    auto operator->() const -> T*;
+    auto get() -> T*;
+    void reset();
+    void reset(T* ptr);
+    void swap(SharedPtr& r);
+
+    // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+    auto use_count() const -> size_t;
+
+
+    T* p_obj;
+    static map<long long, atomic_size_t> ptr_map;
+};
+
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,101 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+
+template<typename T>
+SharedPtr<T>::SharedPtr() {
+    p_obj = nullptr;
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(T *ptr) {
+    p_obj = ptr;
+    ptr_map[static_cast<long long>(p_obj)]++;
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr &r) {
+    p_obj = r.p_obj;
+    ptr_map[static_cast<long long>(p_obj)]++;
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(SharedPtr &&r) {
+    p_obj = r.p_obj;
+    ptr_map[static_cast<long long>(p_obj)]++;
+}
+
+template<typename T>
+SharedPtr<T>::~SharedPtr() {
+    ptr_map[static_cast<long long>(p_obj)]--;
+    if (ptr_map[static_cast<long long>(p_obj)] == 0) {
+        ptr_map.erase(static_cast<long long>(p_obj));
+        delete p_obj;
+    }
+}
+
+template<typename T>
+auto SharedPtr<T>::operator=(const SharedPtr &r) -> SharedPtr & {
+    if (p_obj != r.p_obj) {
+        reset(r.p_obj);
+    }
+    return *this;
+}
+
+template<typename T>
+auto SharedPtr<T>::operator=(SharedPtr &&r) -> SharedPtr & {
+    if (p_obj != r.p_obj) {
+        reset(r.p_obj);
+    }
+    return *this;
+}
+
+// проверяет, указывает ли указатель на объект
+template<typename T>
+SharedPtr<T>::operator bool() const {
+    if (!p_obj) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+template<typename T>
+auto SharedPtr<T>::operator*() const -> T & {
+    return *p_obj;
+}
+
+template<typename T>
+auto SharedPtr<T>::operator->() const -> T * {
+    return p_obj;
+}
+
+template<typename T>
+auto SharedPtr<T>::get() -> T * {
+    return *p_obj;
+}
+
+template<typename T>
+void SharedPtr<T>::reset() {
+    ~SharedPtr();
+    this->SharedPtr();
+}
+
+template<typename T>
+void SharedPtr<T>::reset(T *ptr) {
+    ~SharedPtr();
+    this->SharedPtr(ptr);
+}
+
+template<typename T>
+void SharedPtr<T>::swap(SharedPtr &r) {
+    T *tmp = p_obj;
+    p_obj = r.p_obj;
+    r.p_obj = tmp;
+}
+
+// возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+template<typename T>
+auto SharedPtr<T>::use_count() const -> size_t {
+    return ptr_map[static_cast<long long>(p_obj)];
+}
