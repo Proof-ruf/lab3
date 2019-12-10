178ca48f68a3542d33eb357fc441179bce48c0cc
Ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-10 23:04:05 +0300

    1.4

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,5 +3,31 @@
 #include <gtest/gtest.h>
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+int a = 7;
+int b = 8;
+
+SharedPtr<int> s1(&a);
+EXPECT_EQ(s1.use_count(), 1);
+EXPECT_EQ(s1.get(), &a);
+
+SharedPtr<int> s2(&a);
+SharedPtr<int> s3(&b);
+
+EXPECT_EQ(*s1, a);
+s1.swap(s2);
+EXPECT_EQ(s1, s2);
+
+s1.reset();
+EXPECT_EQ(s1.use_count, 0);
+
+s1 = s2;
+EXPECT_EQ(s1, s2);
+
+s1.reset(&b);
+EXPECT_EQ(s1.use_count, s3.use_count);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
